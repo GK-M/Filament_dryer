e38f2a2bd4b152867765d41ca9f672ee1c73fd9c
G__K <93984678+GK-M@users.noreply.github.com>
2026-06-25 13:57:08 +0200

    Add PlatformIO project files

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#include <Arduino.h>
+#include <WiFi.h>
+
+
+#include "config.h"
+#include "secrets.h"
+
+// Ctrl + Shift + Space <- Podpowiedz
+
+
+// put function declarations here:
+int myFunction(int, int);
+
+void setup() {
+WiFi.begin()
+  // put your setup code here, to run once:
+  int result = myFunction(2, 3);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
+}
